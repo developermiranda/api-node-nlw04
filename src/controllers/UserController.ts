b4de8f95f4ff1d08a537358fb5d8c6b75603ef59
Daniel Miranda <developermiranda@gmail.com>
2021-02-23 18:15:35 -0300

    Segunda etapa concluida

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
+
+class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+    // console.log(body);
+
+    const usersRepository = getRepository(User);
+
+    // select * from users where email = "email"
+    const userAlreadyExists = await usersRepository.findOne({ email })
+
+    // definimos a regra de negócio, para verificar se o usuário já existe
+    if(userAlreadyExists) {
+      return response.status(400).json({ error: "User already exists!" });
+    }
+
+
+    const user = usersRepository.create({
+      name, email
+    })
+
+    await usersRepository.save(user);
+
+    return response.json(user);
+  }
+}
+
+export { UserController }
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,11 @@
-import express, { response } from 'express'
+import "reflect-metadata";
+import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
+app.use(express.json())
 
-// Métodos de requição
-// GET => Buscar
-// POST => Salvar
-// PUT => Alterar
-// DELETE => Deletar
-// PATCH  => Alteração específica
-
-//http://localhost:3333/users
-
-app.get("/", (request, response) => {
-  return response.json({message: "Hello wold - NLW04"})
-})
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-
-app.post("/", (request, response)=>{
-  // Recebeu os dados para salvar
-  return response.json({message: "Os dados foram salvos com sucesso!"})
-})
-
+app.use(router);
 
 app.listen(3333, () => console.log("Server is  running"))
